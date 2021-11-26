3b69439004a78cbc12364698ad11dcafb9f52944
AmirSaleki <amir.saleki8@gmail.com>
2021-11-26 20:20:41 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [global, setGlobal] = useState();
+  const [country, setCountry] = useState("");
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  const [todoList, setTodoList] = useState([]);
+  const [task, setTask] = useState("");
+
+  const countryHandler = (e) => {
+    setSearchedCountry(e.target.value);
+  };
+
+  async function getData() {
+    const response = await fetch(
+      "https://coronavirus-19-api.herokuapp.com/countries"
+    );
+    const result = await response.json();
+    setGlobal(result[0]);
+    result.forEach((element) => {
+      element.country.toLowerCase().trim() === searchedCountry.toLowerCase() &&
+        setCountry(element);
+    });
+  }
+
+  const addTaskHandler = (e) => {
+    setTask(e.target.value);
+  };
+  const todoListHandler = () => {
+    setTodoList([...todoList, task]);
+  };
+  const removeTaskHandler = (e) => {
+    setTodoList(todoList.filter((item) => item !== e.target.value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Tools</h1>
+        <p>Created by Amir Saleki</p>
+      </div>
+      <div className="content">
+        <div className="fetchData">
+          <h2>Corona Stats</h2>
+          <div className="fetchHeader">
+            <input
+              onKeyDown={getData}
+              onChange={countryHandler}
+              type="text"
+              placeholder="Enter country name"
+            />
+            <button onClick={getData}>Find</button>
+          </div>
+          <div className="fetchResult">
+            {global && (
+              <div>
+                <h3>Global Statistics</h3>
+                <h5>Active Cases: {global.active}</h5>
+                <h5>Total Cases: {global.cases}</h5>
+                <h5>Critical Cases: {global.critical}</h5>
+                <h5>Total Death: {global.deaths}</h5>
+                <h5>Today Cases: {global.todayCases}</h5>
+                <h5>Today Deaths: {global.todayDeaths}</h5>
+              </div>
+            )}
+            {country !== "" && (
+              <div>
+                <hr style={{ margin: "1rem" }} />
+                <h3>{country.country} Statistics</h3>
+                <h5>Active Cases: {country.active}</h5>
+                <h5>Total Cases: {country.cases}</h5>
+                <h5>Critical Cases: {country.critical}</h5>
+                <h5>Total Death: {country.deaths}</h5>
+                <h5>Today Cases: {country.todayCases}</h5>
+                <h5>Today Deaths: {country.todayDeaths}</h5>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="todo">
+          <h2>Todo List</h2>
+          <input onChange={addTaskHandler} type="text" placeholder="Task" />
+          <button onClick={todoListHandler}>Add</button>
+          {todoList &&
+            todoList.map((item) => (
+              <div
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                }}
+                key={item}
+              >
+                <h3>{item}</h3>
+                <button
+                  value={item}
+                  onClick={removeTaskHandler}
+                  style={{ padding: "0.5rem" }}
+                >
+                  done
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,36 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/hey yo/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App Component Tests", () => {
+  test("renders the 'Todo List'", () => {
+    render(<App />);
+    const text = screen.getByText("Todo List");
+    expect(text).toBeInTheDocument();
+  });
+
+  test("renders 'Buy Milk' after adding 'Buy Milk' to todo list", () => {
+    render(<App />);
+    const editText = screen.getByPlaceholderText("Task");
+    const button = screen.getByText("Add");
+    userEvent.type(editText, "Buy Milk");
+    userEvent.click(button);
+    const result = screen.getByText("Buy Milk");
+    expect(result).toBeInTheDocument();
+  });
+
+  test("renders global corona stats", async () => {
+    render(<App />);
+    const button = screen.getByText("Find");
+    userEvent.click(button);
+    //only find... methods return a promise
+    const coronaStat = await screen.findByText("Global Statistics");
+    expect(coronaStat).toBeInTheDocument();
+  });
 });
